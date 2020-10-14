7dbd5c68b71543543d9dd97d33ce35ae15b052b3
mohamed saber <msaber.shi@outlook.com>
2020-10-14 23:52:45 +0200

    Création du formulaire Ajout message

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ router.get('/', (req, res)=>{
     res.render('tweets/tweet-list')
 })
 
+router.get('/tweet/new', (req, res)=>{
+    res.render('tweets/tweet-form')
+})
+
 
 module.exports = router;
